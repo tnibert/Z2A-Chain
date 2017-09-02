7fb7b3fc3f0de272df4124836a26d054ebf210df
Tim Nibert <docz2a@gmail.com>
2017-09-02 18:18:42 +1000

    added block and chain validation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# to start we'll follow this tutorial: http://blockxchain.org/2017/06/04/building-a-blockchain-with-python-1/
 #! /usr/bin/env python
 import hashlib
+import time
 
 class Block:
     def __init__(self, index, previousHash, timestamp, data, currentHash):
@@ -24,3 +26,54 @@ def calculateHash(index, previousHash, timestamp, data):
 def calculateHashForBlock(block):
     return calculateHash(block.index, block.previousHash, block.timestamp, block.data)
 
+def getLatestBlock():
+    return blockchain[len(blockchain)-1]
+
+def generateNextBlock(blockData):
+    previousBlock = getLatestBlock()
+    nextIndex = previousBlock.index + 1
+    nextTimestamp = time.time()
+    nextHash = calculateHash(nextIndex, previousBlock.currentHash, nextTimestamp, blockData)
+    return Block(nextIndex, previousBlock.currentHash, nextTimestamp, nextHash)
+
+# check if two blocks are the same
+# we can move this into a method of the block class
+def isSameBlock(block1, block2):
+    if block1.index != block2.index:
+        return False
+    elif block1.previousHash != block2.previousHash:
+        return False
+    elif block1.timestamp != block2.timestamp:
+        return False
+    elif block1.data != block2.data:
+        return False
+    elif block1.currentHash != block2.currentHash:
+        return False
+    return True
+
+# determine if new block is valid
+def isValidNewBlock(newBlock, previousBlock):
+    if previousBlock.index + 1 != newBlock.index:
+        print('Indices Do Not Match Up')
+        return False
+    elif previousBlock.currentHash != newBlock.previousHash:
+        print("Previous hash does not match")
+        return False
+    elif calculateHashForBlock(newBlock) != newBlock.hash:
+        print("Hash is invalid")
+        return False
+    return True
+
+# iterate over entire list to check chain
+def isValidChain(bcToValidate):
+    if not isSameBlock(bcToValidate[0], getGenesisBlock()):
+        print('Genesis Block Incorrect')
+        return False
+
+    tempBlocks = [bcToValidate[0]]
+    for i in range(1, len(bcToValidate)):
+        if isValidNewBlock(bcToValidate[i], tempBlocks[i-1]):
+            tempBlocks.append(bcToValidate[i])
+        else:
+            return False
+    return True
