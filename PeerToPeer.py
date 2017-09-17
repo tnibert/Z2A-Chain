b522fb689330988bc21a9235a9d4c8be0dd3f3d4
Tim Nibert <docz2a@gmail.com>
2017-09-17 14:53:57 +1000

    debug messages and fiddling

--- PeerToPeer.py
+++ PeerToPeer.py
@@ -20,6 +20,7 @@ class MyProtocol(Protocol):
         self.state = "HELLO"
         self.remote_nodeid = None
         self.nodeid = self.factory.nodeid
+        print("Protocol init")
 
     def connectionMade(self):
         print("Connection from", self.transport.getPeer())
@@ -31,6 +32,7 @@ class MyProtocol(Protocol):
         print(self.nodeid, "disconnected")
 
     def dataReceived(self, data):
+        print("protocol datareceived")
         for line in data.splitlines():
             line = line.strip()
             if self.state == "HELLO":
@@ -38,10 +40,12 @@ class MyProtocol(Protocol):
                 self.state = "READY"
 
     def send_hello(self):
+        print("protocol send_hello")
         hello = json.puts({'nodeid': self.nodeid, 'msgtype': 'hello'})
         self.transport.write(hello + "\n")
 
     def handle_hello(self, hello):
+        print("protocol handle_hello")
         hello = json.loads(hello)
         self.remote_nodeid = hello["nodeid"]
         if self.remote_nodeid == self.nodeid:
@@ -53,27 +57,38 @@ class MyProtocol(Protocol):
 
 class MyFactory(Factory):
 
-    def __init__(self):
-        self.nodeid = generate_nodeid()
+#    def __init__(self):
+#        print("factory init")
+#        self.nodeid = generate_nodeid()
+#        super.__init__()
 
     def startFactory(self):
+        print("factory startFactory")
         self.peers = {}
-        #self.nodeid = generate_nodeid()
+        self.nodeid = generate_nodeid()
 
     def buildProtocol(self, addr):
+        print("factory buildProtocol")
         return MyProtocol(self)
 
 
 endpoint = TCP4ServerEndpoint(reactor, 5999)
 endpoint.listen(MyFactory())
 
+print(endpoint)
+
 from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
 
 def gotProtocol(p):
     """The callback to start the protocol exchange. We let connecting
     nodes start the hello handshake"""
+    print("gotProtocol, sending hello")
     p.send_hello()
 
+clientFactory = MyFactory()
+clientFactory.startFactory()
+
 point = TCP4ClientEndpoint(reactor, "localhost", 5999)
-d = connectProtocol(point, MyProtocol(MyFactory()))
+d = connectProtocol(point, MyProtocol(clientFactory))
+print(d)
 d.addCallback(gotProtocol)
